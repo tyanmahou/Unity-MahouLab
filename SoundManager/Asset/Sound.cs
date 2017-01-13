1dd388288077dab1283f33dcce6f17276612ac73
mahou <>
2017-01-14 05:03:01 +0900

    soundmanager and fadeManager add

---
+++ SoundManager/Asset/Sound.cs
@@ -0,0 +1,93 @@
+﻿
+using UnityEngine;
+
+//音楽再生class
+public class Sound
+{
+    //hierarchyでまとめあげる親
+    static GameObject m_parentObject;
+
+    //AuioSource用のゲームオブジェクト
+    GameObject m_soundObject;
+
+    AudioClip m_clip;
+    AudioSource m_source;
+
+    public Sound(string path)
+    {
+        if (m_parentObject==null)
+        {
+            m_parentObject = new GameObject("Sound");
+        }
+        if (m_soundObject==null)
+        {
+            m_soundObject = new GameObject(path);
+            m_soundObject.transform.parent = m_parentObject.transform;
+            m_soundObject.AddComponent<AudioSource>();
+        }
+        m_source = m_soundObject.GetComponent<AudioSource>();
+
+        m_clip = Resources.Load(path) as AudioClip;
+        if (m_clip == null)
+        {
+            Debug.Log("LoadError:" + path);
+            return;
+        }
+        m_source.clip = m_clip;
+    }
+
+    //再生
+    public void Play()
+    {
+        if(!m_source.isPlaying)
+        m_source.Play();
+    }
+
+    //停止
+    public void Stop()
+    {
+        m_source.Stop();
+    }
+
+    //一時停止
+    public void Pause()
+    {
+        m_source.Pause();
+    }
+
+    //複数再生
+    public void PlayMulti(float volume=1.0f)
+    {
+        m_source.PlayOneShot(m_clip, volume);
+    }
+    //ループ設定
+    public void SetLoop(bool loop=true)
+    {
+        m_source.loop = loop;
+    }
+
+    //bool演算子
+    //ロード失敗していたら falseが返る
+    public static explicit operator bool(Sound s)
+    {
+        return s.m_clip != null;
+    }
+
+    //ゲッター
+    public AudioSource audioSource
+    {
+        get
+        {
+            return m_source;
+        }
+    }
+    public AudioClip clip
+    {
+        get
+        {
+            return m_clip;
+        }
+    }
+
+
+}
---
+++ SoundManager/Asset/SoundManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+
+public class SoundManager
+{
+    class SoundData
+    {
+        public string m_path;
+        public Sound m_obg;
+
+        public SoundData(string path)
+        {
+            m_path = path;
+        }
+        public void Load()
+        {
+            if(m_obg == null||m_obg.audioSource==null)
+            {
+                m_obg = new Sound(m_path);
+            }
+        }
+    }
+    static Dictionary<string, SoundData> m_factory = new Dictionary<string, SoundData>();
+
+    //サウンドデータの登録
+    public static bool Register(string name, string path)
+    {
+        if (m_factory.ContainsKey(name))
+            return false;
+
+        m_factory.Add(name, new SoundData(path));
+
+        return true;
+    }
+
+    //サウンドデータの取得
+    public static Sound Get(string name)
+    {
+        if (m_factory.ContainsKey(name) == false)
+        {
+            return null;
+        }
+        m_factory[name].Load();
+
+        return m_factory[name].m_obg;
+    }
+}
